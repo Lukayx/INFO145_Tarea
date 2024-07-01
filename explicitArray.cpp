aa3aef18e5321a72e5a9ff57043a7d6d9879ed10
Lagartin1 <andreslabrin10@gmail.com>
2024-06-30 20:23:48 -0400

    dejando como estaba las impresiones

--- explicitArray.cpp
+++ explicitArray.cpp
@@ -76,31 +76,31 @@ int main(int argc, char *argv[]) {
     vector<int> arrayLineal = crearArrayLineal(size, epsilon);
     vector<int> arrayNormal = generarArregloNormal(size, media, desviacionEstandar);
 
-     // Imprimir el arreglo lineal
-    cout << "Array Lineal:" << endl;
-     for (auto i : arrayLineal) {
-        cout << i << " ";
-     }
-     cout << endl;
+    // Imprimir el arreglo lineal
+    //cout << "Array Lineal:" << endl;
+    // for (auto i : arrayLineal) {
+    //    cout << i << " ";
+    // }
+    //cout << endl;
 
     // Imprimir el arreglo normal
-     cout << "Array Normal:" << endl;
-     for (auto i : arrayNormal) {
-         cout << i << " ";
-     }
-    cout << endl;
+    //cout << "Array Normal:" << endl;
+    // for (auto i : arrayNormal) {
+    //   cout << i << " ";
+    // }
+    //cout << endl;
 
 
     // Medición experimental en el arreglo lineal
     int numeroLineal = arrayLineal[rand() % size]; // Número a buscar
     double tarrayLineal = medirTiempo(arrayLineal, numeroLineal); // Inicia búsqueda binaria y mide el tiempo
-    cout << "num: "<< numeroLineal<<"Binary Search en Array lineal de largo " << size 
+    cout << numeroLineal<<"Binary Search en Array lineal de largo " << size 
             << " tarda: " << fixed << setprecision(10) << tarrayLineal << " segundos" << endl;
 
     // Medición experimental en el arreglo normal
     int numeroNormal = arrayNormal[rand() % size]; // Número a buscar
     double tarrayNormal = medirTiempo(arrayNormal, numeroNormal); // Inicia búsqueda binaria y mide el tiempo
-    cout<< "num: " << numeroNormal << "Binary Search en Array normal de largo " << size 
+    cout << numeroNormal << "Binary Search en Array normal de largo " << size 
             << " tarda: " << fixed << setprecision(10) << tarrayNormal << " segundos" << endl;
     return 0;
 }
