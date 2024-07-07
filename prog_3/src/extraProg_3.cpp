4b596036bf4da522cbbfc00b48ba22b4b6ef963a
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-07 19:32:39 -0400

    Update extraProg_3.cpp

--- prog_3/src/extraProg_3.cpp
+++ prog_3/src/extraProg_3.cpp
@@ -2,8 +2,19 @@
 
 // Función que liberar la memoria 
 void deleteHuffmanTree(nodeH* root) {
-  if (!root) return;
+  if (root == nullptr) return;
   deleteHuffmanTree(root->izq);
   deleteHuffmanTree(root->der);
   delete root;
+}
+// Imprime vector
+void printVector(const std::vector<int>& vec) {
+  std::cout << "[";
+  for (size_t i = 0; i < vec.size(); ++i) {
+      std::cout << vec[i];
+      if (i != vec.size() - 1) {
+          std::cout << ", ";
+      }
+  }
+  std::cout << "]" << std::endl;
 }
