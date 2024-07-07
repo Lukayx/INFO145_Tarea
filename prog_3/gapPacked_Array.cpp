d8db1fbc88617957a8a3b37abcb492752eeafb07
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-07 19:32:01 -0400

    Update gapPacked_Array.cpp

--- prog_3/gapPacked_Array.cpp
+++ prog_3/gapPacked_Array.cpp
@@ -7,17 +7,10 @@ int main() {
   size_t size = 100000000;      // Tamaño del arreglo lineal
   int epsilon = 10;  
 
-  std::vector<int> gapCodingArray; // Creación de los arreglos
+  std::vector<int> gapCodingArray; // Creación de los arreglo
   creaArreglos(gapCodingArray, size, epsilon);
   
-  int valor_a_buscar = 83563; 
-  std::cout << "Escriba el valor que quiere buscar: " << valor_a_buscar << std::endl;
-
-  int indice = -1; // Temporalmente, porque la función buscarIndice no está definida
-
-  std::cout << "El índice donde está ese valor es el " << indice << std::endl;
-  
-  std::vector<int> frecs(256, 0); // Supongamos que frecs es un vector de frecuencias de 256 caracteres posibles
+  std::vector<int> frecs(256, 0); // Vector para las frecuencias de los caracteres
   
   for (char ch : gapCodingArray) { // Llena el vector frecs con las frecuencias de los caracteres según tus datos
     frecs[ch]++;
@@ -25,12 +18,18 @@ int main() {
 
   nodeH* root = HuffmanTree(frecs); // Construcción del árbol de Huffman
   
-  std::unordered_map<char, std::string> huffmanCodes; // Generación de los códigos Huffman
-  generateHuffmanCodes(root, "", huffmanCodes);
+  std::unordered_map<char, std::string> huffmanCodes; // Mapa para los códigos Huffman
+  generateHuffmanCodes(root, "", huffmanCodes); // Generación de los códigos Huffman
+  
+  std::vector<int> huffmanCodesInt;
 
-  for (const auto& pair : huffmanCodes) { // Mostrar los códigos Huffman
-    std::cout << pair.first << ": " << pair.second << std::endl;
+  for (const auto& pair : huffmanCodes) {  
+    const std::string& binaryCode = pair.second; 
+    int intValue = std::stoi(binaryCode, nullptr, 2); // Convierte bin en int
+    huffmanCodesInt.push_back(intValue); // Almacena en vector huffmanCodesInt
+    std::cout << pair.first << ": bin = " << binaryCode << " int = " << intValue << std::endl;
   }
+  printVector(huffmanCodesInt);
 
   deleteHuffmanTree(root); // Liberar la memoria del árbol de Huffman
   return EXIT_SUCCESS;
