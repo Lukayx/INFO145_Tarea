e10ad39bddc3402be0e9e9f85f7f31351b37d787
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-08 15:09:44 -0400

    Update extraProg_3.h

--- prog_3/include/extraProg_3.h
+++ prog_3/include/extraProg_3.h
@@ -3,6 +3,8 @@
 #include "struct.h"
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 void deleteHuffmanTree(nodeH* root); // Libera la memoria del árbol de Huffman
 void printVector(const std::vector<int>& vec); // Imprime vector
+std::vector<int> readFile(const std::string& file); //lee archivo 
