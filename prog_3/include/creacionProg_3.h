71546061843bfe839121fa313b898d457a2fdf7d
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-09 23:24:30 -0400

    Update creacionProg_3.h

--- prog_3/include/creacionProg_3.h
+++ prog_3/include/creacionProg_3.h
@@ -2,11 +2,13 @@
 
 #include "struct.h"
 #include <vector>
-#include <string>
-#include <queue>
-#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <bitset>
 
-nodeH* HuffmanTree(const std::unordered_map<int, int>& frecs); // Funcion que construye el árbol de Huffman
-nodeH* createNode(char data, int frec); // Función que crea los nodos del árbol de Huffman
-void generateHuffmanCodes(nodeH* root, std::string code, std::unordered_map<int, std::string>& huffmanCodes); // Funcion que genera los códigos de Huffman
-bool compareNodes(nodeH* a, nodeH* b); // Función que compara las frecs de nodos
+void deleteHuffmanTree(nodeH* root); // Libera la memoria del árbol de Huffman
+void printVector(const std::vector<int>& vec); // Imprime vector
+std::vector<int> readFile(const std::string& file); // Lee archivo txt
+void writeToFile(const std::string& fileName, const std::vector<int>& data); // Crea un archivo compressed_array.txt 
+std::vector<int> calcularBits(const std::vector<int>& huffmanCodesInt); // Función para calcular los bits necesarios para cada elemento
+void escribirBitsEnArchivo(const std::vector<int>& bitsArray, const std::string& fileName); // Funcion que escribe los bits del array comprimido
