31393ac46392a7786d36273b4be32c1604298712
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-09 23:26:54 -0400

    Update extraProg_3.cpp

--- prog_3/src/extraProg_3.cpp
+++ prog_3/src/extraProg_3.cpp
@@ -8,7 +8,7 @@ void deleteHuffmanTree(nodeH* root) {
   delete root;
 }
 
-// Imprime vector
+// Imprime vectores
 void printVector(const std::vector<int>& vec) {
   std::cout << "[";
   for (size_t i = 0; i < vec.size(); ++i) {
@@ -37,4 +37,42 @@ std::vector<int> readFile(const std::string& file) {
   archivo.close();
 
   return datos;
+}
+
+void writeToFile(const std::string& fileName, const std::vector<int>& data) {
+  std::ofstream outFile(fileName);
+  if (!outFile.is_open()) {
+    std::cerr << "Error al abrir el archivo para escribir: " << fileName << std::endl;
+    return;
+  }
+  for (int num : data) {
+    outFile << num << ","; // Escribe cada número seguido de un espacio
+  }
+  outFile.close();
+}
+
+// Función para calcular los bits necesarios para cada elemento
+std::vector<int> calcularBits(const std::vector<int>& huffmanCodesInt) {
+  std::vector<int> bitsArray;
+  for (int num : huffmanCodesInt) {
+    int bits = 0;
+    if (num != 0) {
+      bits = std::bitset<sizeof(num) * 8>(num).to_string().find('1') + 1;
+    }
+    bitsArray.push_back(bits);
+  }
+  return bitsArray;
+}
+
+// Función para escribir los bits en un archivo
+void escribirBitsEnArchivo(const std::vector<int>& bitsArray, const std::string& fileName) {
+    std::ofstream outFile(fileName);
+    if (!outFile.is_open()) {
+        std::cerr << "Error al abrir el archivo para escribir: " << fileName << std::endl;
+        return;
+    }
+    for (int bits : bitsArray) {
+        outFile << bits << std::endl;
+    }
+    outFile.close();
 }
