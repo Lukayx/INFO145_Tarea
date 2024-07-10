fbed7911f2df609e23101c9ef390db5b869f73a8
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-09 23:27:47 -0400

    Update gapPacked_Array.cpp

--- prog_3/gapPacked_Array.cpp
+++ prog_3/gapPacked_Array.cpp
@@ -3,30 +3,63 @@
 #include "include/creacionProg_3.h"
 
 int main() {
-  
-  std::vector<int> gapCodingArray = readFile("DISTRIBUCION_LINEAL.txt"); // Llama a la función para leer datos desde archivo
 
-  std::vector<int> frecs(256, 0); // Vector para las frecuencias de los caracteres
-  
-  for (char ch : gapCodingArray) { // Llena el vector frecs con las frecuencias de los caracteres según tus datos
-    frecs[ch]++;
+  std::vector<int> gapCodingArray = readFile("DISTRIBUCION_LINEAL1.txt"); // Llama a la función para leer datos desde archivo
+
+  std::unordered_map<int, int> frecs; 
+  for (int num : gapCodingArray) {  // Llena el mapa frecs con las frecuencias de los enteros dek gapCodingArray
+    frecs[num]++;
   }
 
-  nodeH* root = HuffmanTree(frecs); // Construcción del árbol de Huffman
+  nodeH* root = HuffmanTree(frecs); // Construye del árbol de Huffman
   
-  std::unordered_map<char, std::string> huffmanCodes; // Mapa para los códigos Huffman
-  generateHuffmanCodes(root, "", huffmanCodes); // Generación de los códigos Huffman
-  
-  std::vector<int> huffmanCodesInt;
+  std::unordered_map<int, std::string> huffmanCodes; // Generación de los códigos Huffman
+  generateHuffmanCodes(root, "", huffmanCodes); 
 
+  std::unordered_map<int, int> huffmanCodesIntMap;  // Almacena 
+  std::unordered_map<int, int> huffmanCodeLengths;  
+
+  std::vector<int> huffmanCodesInt;
   for (const auto& pair : huffmanCodes) {  
     const std::string& binaryCode = pair.second; 
     int intValue = std::stoi(binaryCode, nullptr, 2); // Convierte bin en int
-    huffmanCodesInt.push_back(intValue); // Almacena en vector huffmanCodesInt
+    huffmanCodesIntMap[pair.first] = intValue;
+    huffmanCodeLengths[pair.first] = binaryCode.length();
+    huffmanCodesInt.push_back(intValue);  // Almacena en vector huffmanCodesInt
     std::cout << pair.first << ": bin = " << binaryCode << " int = " << intValue << std::endl;
   }
+
+  std::vector<int> compressedArray; 
+  int originalBits = 0;
+  int compressedBits = 0; 
+  
   printVector(huffmanCodesInt);
 
+  for (int num : gapCodingArray) {
+    compressedArray.push_back(huffmanCodesInt[num]);
+    originalBits += 32; // Suma bits gapCodingArray
+    compressedBits += huffmanCodeLengths[num]; // Suma bits compressedArray
+  }
+  
+  writeToFile("compressed_array.txt", compressedArray); // Escribe el arreglo comprimido a un archivo de texto
+
+  std::cout << "Bits gapCodingArray: " << originalBits << std::endl;  
+  std::cout << "Bits compressedArray: " << compressedBits << std::endl;
+
+  std::vector<int> bitsArray = calcularBits(huffmanCodesInt);// Calcular los bits necesarios para cada elemento
+  escribirBitsEnArchivo(bitsArray, "bits_huffmanCodesInt.txt");
+
   deleteHuffmanTree(root); // Liberar la memoria del árbol de Huffman
+  std::cout << "Listo" << std::endl;
+
+  std::ofstream bitsFile("bits_comparison.txt"); // Escribir la información de bits a un archivo para análisis en Python
+  if (bitsFile.is_open()) {
+    bitsFile << "OriginalBits CompressedBits\n";
+    bitsFile << originalBits << " " << compressedBits << "\n";
+    bitsFile.close();
+  } else {
+    std::cerr << "Error al abrir el archivo para escribir los bits." << std::endl;
+  }
+
   return EXIT_SUCCESS;
 }
