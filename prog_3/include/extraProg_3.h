6756168ad5c7696e5695a73b2afe149036bc1281
l0w3nz0 <93956295+lorenzoVera@users.noreply.github.com>
2024-07-09 23:25:24 -0400

    Update extraProg_3.h

--- prog_3/include/extraProg_3.h
+++ prog_3/include/extraProg_3.h
@@ -4,7 +4,11 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <bitset>
 
 void deleteHuffmanTree(nodeH* root); // Libera la memoria del árbol de Huffman
 void printVector(const std::vector<int>& vec); // Imprime vector
-std::vector<int> readFile(const std::string& file); //lee archivo 
+std::vector<int> readFile(const std::string& file); // Lee archivo txt
+void writeToFile(const std::string& fileName, const std::vector<int>& data); // Crea un archivo compressed_array.txt 
+std::vector<int> calcularBits(const std::vector<int>& huffmanCodesInt); // Función para calcular los bits necesarios para cada elemento
+void escribirBitsEnArchivo(const std::vector<int>& bitsArray, const std::string& fileName); // Funcion que escribe los bits del array comprimido
